5140fea74d57f56ac1f239e065e8a0c457d612fa
Richard Rzeszutek <richengguy@users.noreply.github.com>
2020-07-20 21:38:31 -0400

    Split up the imagelibrary source file.
    The contents are now in two separate files, making the organization a
    bit more obvious.

---
+++ src/mosaic/_types.py
@@ -0,0 +1,5 @@
+import pathlib
+from typing import Union
+
+
+PathLike = Union[str, pathlib.Path]
---
+++ src/mosaic/imagelibrary/__init__.py
@@ -0,0 +1,3 @@
+# flake8: noqa
+from .library import ImageLibrary
+from .cifar100library import CIFAR100Library
